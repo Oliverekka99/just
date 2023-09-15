ec6ee484a99aed40852b86d40461574c8c79c6cb
Oliver Ekka <97191945+Oliverekka99@users.noreply.github.com>
2023-09-15 14:45:41 +0000

    second commit

--- IT_lab/2.cpp
+++ IT_lab/2.cpp
@@ -6,5 +6,19 @@ using namespace std;
 int main(){
     struct dirent *de;
     DIR *dr = opendir(".");
+    string IgnoreFile = ".exe";
 
+    if(dr == NULL){
+        cout << "Could not open current directory" << endl;
+        return 0;
+    }
+    while ((de = readdir(dr)) != NULL){
+        const char *found = strstr(de->d_name, IgnoreFile.c_str());
+        if(found){
+            continue;
+        }
+        printf("%s\n", de->d_name);
+    }
+    closedir(dr);
+    return 0;
 }
