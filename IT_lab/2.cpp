e5d272c7cffbc84b88c4e1f6c1ef74ad6578d523
Oliver Ekka <97191945+Oliverekka99@users.noreply.github.com>
2023-10-22 19:05:11 +0000

    Adding 2nd experiment

--- IT_lab/2.cpp
+++ IT_lab/2.cpp
@@ -1,10 +1,25 @@
-#include <iostream>
-#include <dirent.h>
-#include <string>
-#include <cstring>
-using namespace std;
-int main(){
-    struct dirent *de;
-    DIR *dr = opendir(".");
+#include<iostream>
+#include<dirent.h>
+#include<string>
+#include<cstring>
+using namespace std; 
 
+int main(){
+    struct dirent *de;  
+    DIR *dr = opendir(".");
+    string IgnoreFile = ".exe";
+
+    if (dr == NULL){
+        printf("Could not open current directory");
+        return 0;
+    }
+    while ((de = readdir(dr)) != NULL){
+        const char *found = strstr(de->d_name, IgnoreFile.c_str());
+        if (found){
+            continue;
+        }
+        printf("%s\n", de->d_name);
+    }
+    closedir(dr);
+    return 0;
 }
